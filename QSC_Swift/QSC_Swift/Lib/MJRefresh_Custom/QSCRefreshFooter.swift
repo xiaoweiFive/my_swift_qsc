34fdcb58d2be89d316e9dea29bc03e39d1ada480
zhangzhenwei <924909997@qq.com>
2018-08-02 14:09:38 +0800

    111

--- QSC_Swift/QSC_Swift/Lib/MJRefresh_Custom/QSCRefreshFooter.swift
+++ QSC_Swift/QSC_Swift/Lib/MJRefresh_Custom/QSCRefreshFooter.swift
@@ -10,6 +10,16 @@ let  QSCRefreshFooterHeight:CGFloat = 44
 
 import UIKit
 
+
+
+
+
+
+
+
+
+
+
 class QSCRefreshFooter: QSCRefreshComponent {
 
     class func footerWithRefreshingBlock(refreshingBlock:@escaping QSCRefreshComponentRefreshingBlock)->Self{
