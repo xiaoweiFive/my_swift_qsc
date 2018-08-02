c17b946ceac02a9d83fdeb5eaaad6c91004b8d0f
zhangzhenwei <924909997@qq.com>
2018-08-02 14:01:56 +0800

    Revert "323424"
    This reverts commit 1bd3a070068862683964f75432355e55e5758361.

--- QSC_Swift/QSC_Swift/Lib/MJRefresh_Custom/QSCRefreshFooter.swift
+++ QSC_Swift/QSC_Swift/Lib/MJRefresh_Custom/QSCRefreshFooter.swift
@@ -10,10 +10,6 @@ let  QSCRefreshFooterHeight:CGFloat = 44
 
 import UIKit
 
-
-
-
-
 class QSCRefreshFooter: QSCRefreshComponent {
 
     class func footerWithRefreshingBlock(refreshingBlock:@escaping QSCRefreshComponentRefreshingBlock)->Self{
